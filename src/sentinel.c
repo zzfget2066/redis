0f40fe3a19b96707442bb7b8cc58d96a09984e5f
zzfget2066 <world_2086@qq.com>
2025-11-07 14:35:31 +0800

    update sentinel

--- src/sentinel.c
+++ src/sentinel.c
@@ -4845,8 +4845,6 @@ void sentinelFailoverWaitStart(sentinelRedisInstance *ri) {
 
 void sentinelFailoverSelectSlave(sentinelRedisInstance *ri) {
     sentinelRedisInstance *slave = sentinelSelectSlave(ri);
-    ri->promoted_slave = slave;
-
     /* We don't handle the timeout in this state as the function aborts
      * the failover or go forward in the next state. */
     if (slave == NULL) {
@@ -4854,27 +4852,31 @@ void sentinelFailoverSelectSlave(sentinelRedisInstance *ri) {
         sentinelAbortFailover(ri);
     } else {
         /* === Custom whitelist check after promoted slave selected === */
-        if (ri->failover_guard_script && slave) {
-          char cmd[512];
-          snprintf(cmd, sizeof(cmd),
-              "%s %s %d %s %s %d %s %d",
-              ri->failover_guard_script,
-              ri->name,
-              SENTINEL_LEADER,
-              "start",
-              ri->addr->ip,
-              ri->addr->port,
-              slave->addr->ip,
-              slave->addr->port
-          );
-          int rc = system(cmd);
-          if (rc != 0) {
-              sentinelEvent(LL_WARNING, "+failover-blocked", ri,
-                  "Failover blocked by whitelist check (promoted slave not allowed)");
-              ri->promoted_slave = NULL;
-              sentinelAbortFailover(ri);
-              return;
-          }
+        if (ri->failover_guard_script) {
+            char cmd[512];
+            snprintf(cmd, sizeof(cmd),
+                "\"%s\" \"%s\" %d \"%s\" \"%s\" %d \"%s\" %d",
+                ri->failover_guard_script,
+                ri->name,
+                SENTINEL_LEADER,
+                "start",
+                ri->addr->ip,
+                ri->addr->port,
+                slave->addr->ip,
+                slave->addr->port
+            );
+
+            int rc = system(cmd);
+            if (WIFEXITED(rc))
+                rc = WEXITSTATUS(rc);
+
+            if (rc != 0) {
+                sentinelEvent(LL_WARNING, "-failover-blocked", ri,
+                    "Failover blocked by whitelist check (promoted slave not allowed)");
+                ri->promoted_slave = NULL;
+                sentinelAbortFailover(ri);
+                return;
+            }
         }
         sentinelEvent(LL_WARNING,"+selected-slave",slave,"%@");
         slave->flags |= SRI_PROMOTED;
